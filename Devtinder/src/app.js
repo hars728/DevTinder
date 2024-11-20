3e7562ac92fc826e5a789bfc27aa9ec61a1bea39
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-11-20 14:40:59 +0530

    adding all rest api's

--- Devtinder/src/app.js
+++ Devtinder/src/app.js
@@ -5,19 +5,6 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
-// app.use("/admin", adminAuth);
-
-// app.use("/admin/seedata", (req, res, next) => {
-//     console.log("admin now verified");
-//     res.send("data ware present");
-//     next();
-
-// })
-
-// app.use("/users", isUserAuth, (req, res, next) => {
-//     console.log("checking the auth second");
-//     res.send("users using there own data");
-// })
 
 
 app.post("/signup", async (req, res) => {
@@ -31,7 +18,49 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const user = await User.find({ email: userEmail });
+        if (user.length === 0) {
+            res.status(404).send("user not found");
+        } else {
+            res.send(user);
+        }
 
+    } catch (err) {
+        res.status(400).send("something went wronge");
+    }
+});
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("something went wrong");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const userfordelete = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userfordelete);
+        res.send("user deleted succesfully");
+    } catch (err) {
+        res.status(400).send("something went wronge");
+    }
+});
+app.patch("/user", async (req, res) => {
+    const userforupdate = req.body.userId;
+    const data = req.body;
+    console.log(userforupdate);
+    try {
+        const user = await User.findByIdAndUpdate(userforupdate, data);
+        res.send("user update successfully");
+    } catch {
+        res.status(400).send("something went wrong");
+    }
+});
 
 
 connectDB()
